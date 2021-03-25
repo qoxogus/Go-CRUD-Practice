50ec0632cdb7a91fb2a3f430d02ac7417c0b885d
qoxogus <a01034367055@gmail.com>
2021-03-25 16:47:06 +0900

    [Update delete id]

--- controller/delete.go
+++ controller/delete.go
@@ -9,7 +9,7 @@ import (
 func Delete(c *gin.Context) {
 	Crud := &database.Crud{} //테이블
 
-	err := database.DB.Delete(&Crud, "7").Error //idx == 7인 컬럼 지우기
+	err := database.DB.Delete(&Crud, "8").Error //idx == 7인 컬럼 지우기
 	if err != nil {
 		c.JSON(500, gin.H{
 			"status":  "500",
